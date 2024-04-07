8970bc2834290061b13a67edff8cc4a0d44af16c
maseelshah22 <98069253+maseelshah22@users.noreply.github.com>
2024-04-07 17:15:22 -0400

    history works but not in localstorage

--- game_function.js
+++ game_function.js
@@ -15,14 +15,25 @@ function setUpNewGame() {
 		localStorage.setItem("firstGame", false);
 		localStorage.setItem("totGuesses", 0);
 		document.getElementById("submit").disabled = true;
-		localStorage.setItem("correctWords", JSON.stringify(correctWords));
 	}
+
+	if (localStorage.getItem("correctGuesses") != 4) {
+		localStorage.setItem("streak", 0);
+	}
+
+	document.getElementById("shuffle").disabled = false;
+
+
+	document.getElementById("guessHistory").innerHTML = " ";
+	localStorage.setItem("history", JSON.stringify([]));
+	localStorage.setItem("correctWords", JSON.stringify([]));
 	clickedCount = 0;
 	document.getElementById("submit").disabled = true;
 	localStorage.setItem("categories", null);
 	localStorage.setItem("catToWords", null);
 	localStorage.setItem("wordsToCat", null);
 	localStorage.setItem("board", false);
+	localStorage.setItem("correctGuesses", 0);
 
 	catToWords = new Map();
 	wordArr = [];
@@ -42,6 +53,7 @@ function makeNewBoard(newCategories) {
 }
 
 function createTable(categories) {
+	clickedCount = 0;
 	document.getElementById("submit").disabled = true;
 	wordArr = [];
 	const table = document.createElement("table");
@@ -72,7 +84,10 @@ function createTable(categories) {
 		catToWords.set(currCat, tempWordGroup);
 	}
 
-	localStorage.setItem("wordsToCat", JSON.stringify(Array.from(wordsToCat.entries())));
+	localStorage.setItem(
+		"wordsToCat",
+		JSON.stringify(Array.from(wordsToCat.entries()))
+	);
 
 	//let numWords_left = 0;
 
@@ -87,7 +102,7 @@ function createTable(categories) {
 	let wordsLeft = wordArr.filter((word) => !correctWords.includes(word));
 	let rowNum = wordsLeft.length / 4;
 
-	console.log("hi", wordsLeft);
+	//console.log("hi", wordsLeft);
 
 	wordsLeft = wordsLeft.sort(() => Math.random() - 0.5); //shuffles the words
 
@@ -96,7 +111,6 @@ function createTable(categories) {
 		for (let j = 0; j < 4; j++) {
 			const cellIndex = i * 4 + j; //word index
 			if (cellIndex < wordsLeft.length) {
-				
 				const cell = document.createElement("td");
 				cell.innerHTML = wordsLeft[cellIndex];
 				cell.addEventListener("click", clickCell);
@@ -127,7 +141,6 @@ function createTable(categories) {
 function clickCell(e) {
 	// Check if the clicked cell is already selected
 	if (e.target.classList.contains("selected")) {
-		
 		e.target.classList.remove("selected");
 		clickedCount--;
 		document.getElementById("submit").disabled = true;
@@ -143,10 +156,9 @@ function clickCell(e) {
 		}
 	}
 
-	if(clickedCount == 4){
+	if (clickedCount == 4) {
 		document.getElementById("submit").disabled = false;
 	}
-
 }
 function stats() {
 	var statsDiv = document.querySelector(".stats");
@@ -168,7 +180,151 @@ function increaseGamesPlayed() {
 	let gamesPlayed = parseInt(localStorage.getItem("gPlayed"));
 	gamesPlayed += 1;
 	localStorage.setItem("gPlayed", gamesPlayed);
+	localStorage.setItem(
+		"avg",
+		(
+			parseInt(localStorage.getItem("totGuesses")) /
+			localStorage.getItem("gPlayed")
+		).toFixed(2)
+	);
 	stats();
+}
+
+function submitHandler() {
+	// Find all selected cells
+	let clickedBoxes = document.querySelectorAll(".selected");
+	let subWords = [];
+
+	clickedBoxes.forEach((box) => {
+		subWords.push(box.innerHTML);
+	});
+
+	//console.log(JSON.parse(localStorage.getItem("wordsToCat")));
+
+	let wtc = new Map(JSON.parse(localStorage.getItem("wordsToCat"))); //words to cat
+
+	if (
+		wtc.get(subWords[0]) == wtc.get(subWords[1]) &&
+		wtc.get(subWords[1]) == wtc.get(subWords[2]) &&
+		wtc.get(subWords[2]) == wtc.get(subWords[3])
+	) {
+		console.log("Correct!");
+		let correctWords = JSON.parse(localStorage.getItem("correctWords"));
+		subWords.forEach((word) => {
+			correctWords.push(word);
+		});
+		localStorage.setItem("correctWords", JSON.stringify(correctWords));
+		//console.log(correctWords);
+		createTable(JSON.parse(localStorage.getItem("json")));
+
+		localStorage.setItem(
+			"correctGuesses",
+			parseInt(localStorage.getItem("correctGuesses")) + 1
+		);
+		console.log("category: ", wtc.get(subWords[0]));
+
+		const hist = document.getElementById("guessHistory");
+		const histItem = document.createElement("li");
+		histItem.innerHTML =
+			"Correct Guess: " +
+			subWords[0] +
+			", " +
+			subWords[1] +
+			", " +
+			subWords[2] +
+			", " +
+			subWords[3] +
+			" - Category: " +
+			wtc.get(subWords[0]);
+		hist.appendChild(histItem);
+
+		if (parseInt(localStorage.getItem("correctGuesses")) == 4) {
+			localStorage.setItem("wins", parseInt(localStorage.getItem("wins")) + 1);
+			localStorage.setItem(
+				"streak",
+				parseInt(localStorage.getItem("streak")) + 1
+			);
+			localStorage.setItem(
+				"avg",
+				(
+					parseInt(localStorage.getItem("totGuesses")) /
+					localStorage.getItem("gPlayed")
+				).toFixed(2)
+			);
+
+			const hist = document.getElementById("guessHistory");
+			const histItem = document.createElement("li");
+			histItem.innerHTML = "GAME OVER! You Won! Click New Game To Play Again!";
+			hist.appendChild(histItem);
+
+			stats();
+		}
+	} else {
+		let gCats = [];
+
+		for (let i = 0; i < 4; i++) {
+			gCats.push(wtc.get(subWords[i]));
+		}
+
+		let countMap = new Map(); // Object to store counts
+
+		// Loop through the gCats array
+		gCats.forEach((item) => {
+			if (countMap.has(item)) {
+				countMap.set(item, countMap.get(item) + 1);
+			} else {
+				countMap.set(item, 1);
+			}
+		});
+
+		let countArr = [];
+		console.log(countMap);
+
+		for (let [key, value] of countMap) {
+			countArr.push(value);
+		}
+
+		let maxSim = Math.max(...countArr);
+
+		console.log(countMap);
+
+		const hist = document.getElementById("guessHistory");
+		const histItem = document.createElement("li");
+
+		if (maxSim < 2) {
+			histItem.innerHTML =
+				"Incorrect Guess: " +
+				subWords[0] +
+				", " +
+				subWords[1] +
+				", " +
+				subWords[2] +
+				", " +
+				subWords[3];
+			hist.appendChild(histItem);
+			//console.log("Incorrect!");
+		} else {
+			histItem.innerHTML =
+				"Incorrect Guess: " +
+				subWords[0] +
+				", " +
+				subWords[1] +
+				", " +
+				subWords[2] +
+				", " +
+				subWords[3] +
+				"<br> You Had " +
+				maxSim +
+				" Words From The Same Category";
+
+			hist.appendChild(histItem);
+		}
+	}
+
+	localStorage.setItem(
+		"totGuesses",
+		parseInt(localStorage.getItem("totGuesses")) + 1
+	);
 }
 
 let newGameButton = document.getElementById("newgame");
@@ -180,40 +336,6 @@ clearButton.addEventListener("click", clearHistory);
 
 let shuffButton = document.getElementById("shuffle");
 shuffButton.addEventListener("click", createTable);
-
-function submitHandler() {
-    // Find all selected cells
-    let clickedBoxes = document.querySelectorAll('.selected');
-	let subWords = [];
-
-	clickedBoxes.forEach((box) => {
-		subWords.push(box.innerHTML);
-	});
-
-	//console.log(JSON.parse(localStorage.getItem("wordsToCat")));
-
-	let wtc = new Map(JSON.parse(localStorage.getItem("wordsToCat"))); //words to cat
-
-	
-	if(wtc.get(subWords[0]) == wtc.get(subWords[1]) && wtc.get(subWords[1]) == wtc.get(subWords[2]) && wtc.get(subWords[2]) == wtc.get(subWords[3])){
-		console.log("Correct!");
-		let correctWords = JSON.parse(localStorage.getItem("correctWords"));
-		subWords.forEach((word) => {
-			correctWords.push(word);
-		});
-		localStorage.setItem("correctWords", JSON.stringify(correctWords));
-		console.log(correctWords);
-		createTable(JSON.parse(localStorage.getItem("json")));
-		
-	}
-	else{
-		console.log("Incorrect!");
-	}
-
-	
-
-
-}
 
 let submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", submitHandler);
